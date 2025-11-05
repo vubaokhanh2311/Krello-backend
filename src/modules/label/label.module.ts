26c6833f8339c6d598cdd39e83bd9230514c45bd
VuBaoKhanh <153911764+vubaokhannh@users.noreply.github.com>
2025-11-05 09:53:20 +0700

    fix: resolve LabelService dependency injection issue

--- src/modules/label/label.module.ts
+++ src/modules/label/label.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { LabelService } from './label.service';
 
 @Module({
-  providers: [LabelService]
+  providers: [LabelService],
+  exports: [LabelService],
 })
 export class LabelModule {}
